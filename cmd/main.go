cf25934f2145507aea43eaab8db65fbdd0fca48b
Leo <balamuteon@gmail.com>
2025-04-09 13:12:37 +0300

    startingUp

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/balamuteon/todo_restapi"
+	"github.com/balamuteon/todo_restapi/pkg/handler"
+	"github.com/balamuteon/todo_restapi/pkg/repository"
+	"github.com/balamuteon/todo_restapi/pkg/service"
+)
+
+func main() {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
---
+++ pkg/handler/auth.go
@@ -0,0 +1,11 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) signUp(c *gin.Context) {
+	// TODO implement me
+}
+
+func (h *Handler) signIn(c *gin.Context) {
+	// TODO implement me
+}
