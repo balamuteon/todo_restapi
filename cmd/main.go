e4c96ade7f7f6ab4f74ee2b0abc97a7d1eca2f35
Leo <balamuteon@gmail.com>
2025-04-14 10:05:27 +0300

    link database

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"log"
+	"os"
 
-	"github.com/spf13/viper"
-	"github.com/balamuteon/todo_restapi"
+	todo "github.com/balamuteon/todo_restapi"
 	"github.com/balamuteon/todo_restapi/pkg/handler"
 	"github.com/balamuteon/todo_restapi/pkg/repository"
 	"github.com/balamuteon/todo_restapi/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -15,7 +18,23 @@ func main() {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
